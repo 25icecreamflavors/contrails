bdcc9d5426bf03e675e531b4ee56ce89e32dd07e
Alexander <alexamailwork@gmail.com>
2023-08-04 01:56:08 +0300

    Finally added the file to download the models weights

---
+++ models/download_model.py
@@ -0,0 +1,25 @@
+import argparse
+import segmentation_models_pytorch as smp
+
+
+def download_encoder_weights(encoder_name):
+    model = smp.Unet(encoder_name=encoder_name, encoder_weights="imagenet")
+    print(f"Downloading pretrained weights for {encoder_name}...")
+    model.eval()
+    print(f"Pretrained weights for {encoder_name} downloaded successfully!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download pretrained encoder weights for segmentation models."
+    )
+    parser.add_argument(
+        "encoder_names",
+        nargs="+",
+        type=str,
+        help="Names of encoder models to download weights for.",
+    )
+    args = parser.parse_args()
+
+    for encoder_name in args.encoder_names:
+        download_encoder_weights(encoder_name)
